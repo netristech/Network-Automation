bf66588d5b399d43aae49d8d2d26aef01adf4b0c
netristech <admin@netris.us>
2019-12-18 15:03:01 +0000

    fix IP Address regex

--- port_report.py
+++ port_report.py
@@ -8,81 +8,80 @@ import os
 from datetime import datetime
 from getpass import getpass
 from netmiko import Netmiko
-from maclookup import ApiClient
 
-timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+def main():
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
-# get list of IPs to generate port report from
-input_ips = ""
-while input_ips == "":
-    input_ips = raw_input("Enter a space delimited list of the switch IP addresses: ")
+    # get list of IPs to generate port report from
+    input_ips = ""
+    while input_ips == "":
+        input_ips = raw_input("Enter a space delimited list of the switch IP addresses: ")
 
-# gather username and password
-cisco_user = raw_input("Username: ")
-cisco_pass = getpass()
-sys.stdout.write("Please wait while port report is being generated")
-sys.stdout.flush()
+    # gather username and password
+    cisco_user = raw_input("Username: ")
+    cisco_pass = getpass()
+    sys.stdout.write("Please wait while port report is being generated")
+    sys.stdout.flush()
 
-with open(os.getcwd()+'/port_report_'+timestamp+'.csv', 'w') as csv_file, open(os.getcwd()+'/port_report_'+timestamp+'.html', 'w') as html_file:
+    with open(os.getcwd()+'/port_report_'+timestamp+'.csv', 'w') as csv_file, open(os.getcwd()+'/port_report_'+timestamp+'.html', 'w') as html_file:
 
-    report_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    report_writer.writerow(['Hostname', 'Port', 'Description', 'MAC Address', 'VLAN', 'Vendor', 'IP Address'])
-    html_file.write("<html>\n<head>\n<title>Cisco Switchport Report</title>\n")
-    html_file.write("<style>\n* { padding: 8px; margin: 0; border: 0px; border-spacing: 0px; }\ntr { border-bottom: 1px solid #ccc; }\ntr:first-child { font-weight: bold; }\ntr:nth-child(odd) { background-color: #f2f2f2; }\n</style>\n")
-    html_file.write("</head>\n<body>\n<table>\n<tr>\n<td>Hostname</td>\n<td>Interface</td>\n<td>Description</td>\n<td>MAC Address</td>\n<td>VLAN</td>\n<td>Vendor</td>\n<td>IP Address</td>\n</tr>\n")
+        report_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        report_writer.writerow(['Hostname', 'Port', 'Description', 'MAC Address', 'VLAN', 'IP Address'])
+        html_file.write("<html>\n<head>\n<title>Cisco Switchport Report</title>\n")
+        html_file.write("<style>\n* { padding: 8px; margin: 0; border: 0px; border-spacing: 0px; }\ntr { border-bottom: 1px solid #ccc; }\ntr:first-child { font-weight: bold; }\ntr:nth-child(odd) { background-color: #f2f2f2; }\n</style>\n")
+        html_file.write("</head>\n<body>\n<table>\n<tr>\n<td>Hostname</td>\n<td>Interface</td>\n<td>Description</td>\n<td>MAC Address</td>\n<td>VLAN</td>\n<td>IP Address</td>\n</tr>\n")
 
-    for i in input_ips.split():
+        for i in input_ips.split():
 
-        conn = {
-            "host": i.rstrip("\n"),
-            "username": cisco_user,
-            "password": cisco_pass,
-            "device_type": "cisco_ios",
-        }
+            conn = {
+                "host": i.rstrip("\n"),
+                "username": cisco_user,
+                "password": cisco_pass,
+                "device_type": "cisco_ios",
+            }
 
-        try:
-            net_connect = Netmiko(**conn)
-            vers = net_connect.send_command('show version')
-        except:
-            sys.stdout.write("!")
-            sys.stdout.flush()
-        else:
-            hostname = net_connect.find_prompt().split('#')[0]
-            if "Nexus" in vers:
-                int_list = net_connect.send_command("show interface brief")
+            try:
+                net_connect = Netmiko(**conn)
+                vers = net_connect.send_command('show version')
+            except:
+                sys.stdout.write("!")
+                sys.stdout.flush()
             else:
-                int_list = net_connect.send_command("show ip interface brief")
-            for j in int_list.splitlines():
-                if "down" not in j:
-                    int_conf = net_connect.send_command("show run int "+j.split()[0])
-                    if "mode trunk" not in int_conf:
-                        desc = "none"
-                        mac_add = "not found"
-                        vlan_id = "none"
-                        vendor = "not found"
-                        ip_add = "not found"
-                        for k in int_conf.splitlines():
-                            if "description" in k:
-                                desc = k[13:]
-                        mac_list = net_connect.send_command("show mac add int "+j.split()[0])
-                        for l in mac_list.splitlines():
-                            if re.search('([0-9a-fA-F]\.?){12}', l):
-                                if l.split()[0] == "*":
-                                    vlan_id = l.split()[1]
-                                    mac_add = l.split()[2]
-                                else:
-                                    vlan_id = l.split()[0]
-                                    mac_add = l.split()[1]
-                                if mac_add != "not found":
-                                    client = ApiClient('at_HLIetICZRkY5hYSOne4r3TFMK3MA8')
-                                    vendor = client.get_vendor(mac_add)
-                                ip_list = net_connect.send_command("sh ip arp | inc "+mac_add)
-                                if re.search('([0-9]{1,3}\.){3}[0-9]{1,3}', ip_list):
-                                    ip_add = re.search('([0-9]{1,3}\.){3}[0-9]{1,3}', ip_list).group(1).strip()
-                                report_writer.writerow([hostname, j.split()[0], desc, mac_add, vlan_id, ip_add])
-                                html_file.write("<tr>\n<td>"+hostname+"</td>\n<td>"+j.split()[0]+"</td>\n<td>"+desc+"</td>\n<td>"+mac_add+"</td>\n<td>"+vlan_id+"</td>\n<td>"+ip_add+"</td>\n</tr>\n")
-            net_connect.disconnect()
-            sys.stdout.write(".")
-            sys.stdout.flush()
-    html_file.write("</table>\n</body>\n</html>")
-print "\nInventory report has been generated"
+                hostname = net_connect.find_prompt().split('#')[0]
+                if "Nexus" in vers:
+                    int_list = net_connect.send_command("show interface brief")
+                else:
+                    int_list = net_connect.send_command("show ip interface brief")
+                for j in int_list.splitlines():
+                    if "down" not in j and "up" in j:
+                        int_conf = net_connect.send_command("show run int "+j.split()[0])
+                        if "mode trunk" not in int_conf:
+                            desc = "none"
+                            mac_add = "not found"
+                            vlan_id = "none"
+                            vendor = "not found"
+                            ip_add = "not found"
+                            for k in int_conf.splitlines():
+                                if "description" in k:
+                                    desc = k[13:]
+                            mac_list = net_connect.send_command("show mac address interface "+j.split()[0])
+                            for l in mac_list.splitlines():
+                                if re.search('([0-9a-fA-F]\.?){12}', l):
+                                    if l.split()[0] == "*":
+                                        vlan_id = l.split()[1]
+                                        mac_add = l.split()[2]
+                                    else:
+                                        vlan_id = l.split()[0]
+                                        mac_add = l.split()[1]
+                                    ip_list = net_connect.send_command("show ip arp | inc "+mac_add)
+                                    if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_list):
+                                        ip_add = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_list).group(1)
+                                    report_writer.writerow([hostname, j.split()[0], desc, mac_add, vlan_id, ip_add])
+                                    html_file.write("<tr>\n<td>"+hostname+"</td>\n<td>"+j.split()[0]+"</td>\n<td>"+desc+"</td>\n<td>"+mac_add+"</td>\n<td>"+vlan_id+"</td>\n<td>"+ip_add+"</td>\n</tr>\n")
+                net_connect.disconnect()
+                sys.stdout.write(".")
+                sys.stdout.flush()
+        html_file.write("</table>\n</body>\n</html>")
+    print "\nInventory report has been generated"
+
+    main()
