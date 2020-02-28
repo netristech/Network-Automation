92e672ea18c20ec7433d718d4c26b9ee2c1f023e
netristech <admin@netris.us>
2020-02-28 18:15:00 +0000

    changed package manager to yum from apt-get

--- Scripts/discover_devices.sh
+++ Scripts/discover_devices.sh
@@ -3,7 +3,7 @@
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
     nmap -V > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        sudo apt-get install -y -qq nmap > /dev/null 2>&1
+        sudo yum install -y -qq nmap > /dev/null 2>&1
     fi
     oct=$(echo $1 | cut -d'.' -f1)
     sudo nmap -Pn -sT -p 22 --open -n $1 | grep -o "$oct.*" >> $2
