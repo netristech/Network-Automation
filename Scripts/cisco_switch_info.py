4bee3587ee4fba5e0e3a816e3c5f408450b4ae11
netristech <admin@netris.us>
2020-04-23 20:44:41 +0000

    Additional error checking for default route

--- Scripts/cisco_switch_info.py
+++ Scripts/cisco_switch_info.py
@@ -111,12 +111,12 @@ def main():
                     # Build report
                     vars = [sw_role, sw_mac, sw_prio, sw_hwver, sw_ports, sw_model, sw_swver, sw_image, sw_sn]
                     for i in vars:
-                        report_writer.writerow(i[0], 'Switch# 1', i[1])
+                        report_writer.writerow([i[0], 'Switch# 1', i[1]])
                         for j in i[2:]:
-                            report_writer.writerow('', f'Switch# {j + 2}', i[j])
-                    report_writer.writerow('Management Information', 'Hostname', hostname)
-                    report_writer.writerow('', 'IP Address', mgmt_ip)
-                    report_writer.writerow('', 'Gateway', def_route)
+                            report_writer.writerow(['', f'Switch# {j + 2}', i[j]])
+                    report_writer.writerow(['Management Information', 'Hostname', hostname])
+                    report_writer.writerow(['', 'IP Address', mgmt_ip])
+                    report_writer.writerow(['', 'Gateway', def_route])
                     net_connect.disconnect()
                     sys.stdout.write(".")
                     sys.stdout.flush()
