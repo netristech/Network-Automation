88ba5a613bcadfe75658df0f801f172f0cbe6333
netristech <admin@netris.us>
2020-07-07 20:07:50 +0000

    Added script to parse through shutdown ports and bring them up

---
+++ Scripts/ports_up.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#import modules
+import re
+import sys
+import os
+import ipaddress
+from datetime import datetime
+from getpass import getpass
+from netmiko import Netmiko
+
+def main():
+
+    # generate input_file from input_subnets containing all IPs to perform inventory scan against
+    while True:
+        try:
+            input_ips = input("Enter a space delimited list of IPs to scan: ").split()
+            for i in range(0, len(input_ips)):
+                input_ips[i] = ipaddress.IPv4Address(input_ips[i])
+        except:
+            if input("Invalid data entered. Press any key to continue or 'q' to quit. ") == 'q':
+                exit()
+        else:
+            break
+    
+    if len(input_ips) > 0:
+
+        # gather username and password
+        cisco_user = input("Device Username: ")
+        cisco_pass = getpass("Device Password: ")
+        sys.stdout.write("Please wait while inventory report is being generated")
+        sys.stdout.flush()
+
+        for ip in input_ips:
+                # Create connection object for Netmiko
+                conn = {
+                    "host": str(ip),
+                    "username": cisco_user,
+                    "password": cisco_pass,
+                    "device_type": "cisco_ios",
+                }
+
+                # Attempt connection
+                try:
+                    net_connect = Netmiko(**conn)
+                    vers = net_connect.send_command('show version')
+                except:
+                    sys.stdout.write("!")
+                    sys.stdout.flush()
+                else:
+                    
+                    # Special handling for Nexus
+                    if "Nexus" in vers:
+                        ports = net_connect.send_command('show int bri | inc "Administratively"')
+                    # Normal handling for IOS
+                    else:
+                        ports = net_connect.send_command('show ip int bri | inc administratively')
+                    for line in ports.splitlines():
+                        int = line.split()[0]
+                        net_connect.send_config_set([f'interface {int}', f'no shutdown'])
+                    net_connect.save_config()
+                    net_connect.disconnect()
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+        os.remove(f"{os.getcwd()}/ips_{timestamp}")
+main()
+print("\nInventory report has been generated")
