bffad3863e273880051dd0fab2c7f0fef1dc170b
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-05 16:14:28 -0500

    Testing getJSON

--- Web/index.php
+++ Web/index.php
@@ -1,6 +1,27 @@
 <?php
 ?>
-<html>
-<h1>Hello World!</h1>
-<p>Soon this will be a really cool web application. Stay tuned...</p>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Load CSS Libraries -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/chart.min.css">
+
+    <title>Network Automation UI - Dev</title>
+  </head>
+  <body>
+    <div id="sites" class="container">
+    </div>
+
+    <!-- Javascript Libraries -->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/chart.min.js"></script>
+    <script src="/js/index.js"></script>
+
+  </body>
 </html>
---
+++ Web/js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $.getJSON("/data/data.json", function(data){
+        for (i = 0; i < data.length; i++) {
+            $("#sites").append([
+                '<div class="row">',
+                '<div class="col-2">',
+                `${data[i].name}`,
+                '</div>',
+                '<div class="col-5">',
+                `<a href="https://maps.google.com/?q=${encodeURIComponent(data[i].address)}" target="_blank">${data[i].address}</a>`,
+                '</div>',
+                '<div class="col-5">',
+                `${data[i].subnets.toString()}`,
+                '</div>',
+                '</div>'
+            ].join('\n'));
+        }
+    }).fail(function(){
+        console.log("Error reading JSON data file");
+    });
+});
+
+// class for sites
+class Site {
+    constructor(params) {
+        this.name = params.name;
+        this.address = params.address;
+        this.subnets = params.subnets;
+    }
+}
