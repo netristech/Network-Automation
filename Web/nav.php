1f0cbbff87a078cbf4fc38bcbbc812c756c87017
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-07 18:23:13 -0500

    Testing edit functionality

--- Web/nav.php
+++ Web/nav.php
@@ -5,7 +5,7 @@
       <div id ="nav" class="row pt-2">
         <button id="edit-btn" type="button" class="btn btn-primary mr-2 mb-2"><i class="far fa-edit"></i></button>
         <button id="add-btn" type="button" class="btn btn-primary mr-2 mb-2"><i class="fas fa-plus"></i></button>
-        <button id="save-btn" type="button" class="btn btn-success mr-2 mb-2"><i class="far fa-save"></i>&nbsp;Save</button>
+        <button id="save-btn" type="button" class="btn btn-success mr-2 mb-2"><i class="fas fa-save"></i>&nbsp;Save</button>
         <button id="cancel-btn" type="button" class="btn btn-danger mr-2 mb-2"><i class="fas fa-times"></i>&nbsp;Cancel</button>        
         <button id="export-btn" type="button" class="btn btn-secondary mr-2 mb-2"><i class="fas fa-file-export"></i>&nbsp;Export to CSV</button>
       </div>
