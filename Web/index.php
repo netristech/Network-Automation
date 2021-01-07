ae10d6352f3bb1bd5caa01352231f13ed267bdcd
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-07 09:56:10 -0500

    Testing FontAwesome

--- Web/index.php
+++ Web/index.php
@@ -19,7 +19,7 @@
     <div id="sites" class="container">
       <div id ="navigation" class="row pt-2 pb-2">
         <button type="button" class="btn btn-primary mr-2"><i class="far fa-edit"></i></button>
-        <button type="button" class="btn btn-secondary mr-2"><i class="fas fa-file-export"></i>Export to CSV</button>
+        <button type="button" class="btn btn-secondary mr-2"><i class="fas fa-file-export"></i>&nbsp;Export to CSV</button>
       </div>
       <div class="row">
         <table id="sites-table" class="table table-striped">
