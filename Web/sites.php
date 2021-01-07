010c3172bb2f097922f783eb332eccfd02ea44ed
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-07 15:39:51 -0500

    Testing edit functionality

--- Web/sites.php
+++ Web/sites.php
@@ -5,9 +5,9 @@
         <table id="sites-table" class="table table-striped">
           <thead class="thead-light">
             <tr>
-              <th scope="col">Site Name</th>
-              <th scope="col">Address</th>
-              <th scope="col">Management Subnet(s)</th>
+              <th scope="col col-2">Site Name</th>
+              <th scope="col col-5">Address</th>
+              <th scope="col col-5">Management Subnet(s)</th>
             </tr>
           </thead>
           <tbody>
