2b3d9d1b9f929007a6b70437384db574a6b687cf
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-07 11:40:10 -0500

    Break pages into sections and join with PHP

--- Web/index.php
+++ Web/index.php
@@ -1,21 +1,3 @@
 <?php include 'header.php';?>
-    <div id="sites" class="container">
-      <div id ="navigation" class="row pt-2 pb-2">
-        <button type="button" class="btn btn-primary mr-2"><i class="far fa-edit"></i></button>
-        <button type="button" class="btn btn-secondary mr-2"><i class="fas fa-file-export"></i>&nbsp;Export to CSV</button>
-      </div>
-      <div class="row">
-        <table id="sites-table" class="table table-striped">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">Site Name</th>
-              <th scope="col">Address</th>
-              <th scope="col">Management Subnet(s)</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-    </div>
+<?php include 'sites.php';?>
 <?php include 'footer.php';?>
