a15e65c2fd9e1ca18b96a705cb1f05c89483ae12
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-11 11:30:28 -0500

    Testing AJAX File Import Function

--- Web/upload.php
+++ Web/upload.php
@@ -1,38 +1,40 @@
 
 <?php
-$target_dir = "data/";
-$target_file = $target_dir . basename($_FILES["importFile"]["name"]);
-$uploadOk = 1;
-$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  //echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["importFile"]["size"] > 100000) {
-  //echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Make sure file is actually a CSV
-if($fileType != "csv") {
-  //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  //echo "Sorry, your file was not uploaded.";
-  echo 0;
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["importFile"]["tmp_name"], $target_file)) {
-    echo $target_file;
-  } else {
+if(isset($_FILES["file"]) && $_FILES["file"]["error"] == 0) {
+  $target_dir = "data/";
+  $target_file = $target_dir . basename($_FILES["file"]["name"]);
+  $uploadOk = 1;
+  $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    //echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+  
+  // Check file size
+  if ($_FILES["file"]["size"] > 100000) {
+    //echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+  
+  // Make sure file is actually a CSV
+  if($fileType != "csv") {
+    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+  }
+  
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    //echo "Sorry, your file was not uploaded.";
     echo 0;
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+      echo $target_file;
+    } else {
+      echo 0;
+    }
   }
 }
 ?>
