1f3547fa51b2caf054723a8b7f1d7f64468696f5
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-11 11:57:55 -0500

    Testing AJAX File Import Function

--- Web/js/index.js
+++ Web/js/index.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
                     //$('#import-modal').modal('hide');
                     //view = 'sites';
                     //drawScreen();
-                    alert(fd.toString());
+                    alert(fd.data('file').toString());
                 } else {
                     $('#import-form .modal-body').append('<div class="alert alert-danger">Error uploading file, please check file type and size are correct.</div>');
                 }
