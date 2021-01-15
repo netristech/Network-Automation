084202dd170cc2d92f3bd4ddb1d561ff1593b55f
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-15 14:38:32 -0500

    Testing authentication

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,7 @@ include 'header.php';
     <form id="login" action="" method="post" enctype="multipart/formdata">
         <div class="alert alert-danger">Authentication Failed</div>
         <label for="username">Username: </label>
-        <input id="username" type="text" name="username" value="<?= $_POST['username'] ?>" />
+        <input id="username" type="text" name="username" value="" />
         <label for="password">Password: </label>
         <input id="password" type="password" name="password" value="" />
         <input type="submit" name="submit" value="Sign in" class="btn btn-primary" />
