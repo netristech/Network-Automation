c3caf29e2bbfc6d72d4c0b8eb97541a649f6e572
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-15 14:41:17 -0500

    Testing authentication

--- Web/index.php
+++ Web/index.php
@@ -2,12 +2,14 @@
 //session_start();
 include 'header.php';
 ?>
-    <form id="login" action="" method="post" enctype="multipart/formdata">
-        <div class="alert alert-danger">Authentication Failed</div>
-        <label for="username">Username: </label>
-        <input id="username" type="text" name="username" value="" />
-        <label for="password">Password: </label>
-        <input id="password" type="password" name="password" value="" />
-        <input type="submit" name="submit" value="Sign in" class="btn btn-primary" />
-    </form>
+    <div id="login">
+        <form id="login-form" action="" method="post" enctype="multipart/formdata">
+            <div class="alert alert-danger">Authentication Failed</div>
+            <label for="username">Username: </label>
+            <input id="username" type="text" name="username" value="" /><br />
+            <label for="password">Password: </label>
+            <input id="password" type="password" name="password" value="" /><br />
+            <input type="submit" name="submit" value="Sign in" class="btn btn-primary" />
+        </form>
+    </div>
 <?php include 'footer.php'; ?>
