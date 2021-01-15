1bb099bb7d5031e34058f847bc87487b9a8d69dd
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-15 15:41:23 -0500

    Testing authentication

--- Web/index.php
+++ Web/index.php
@@ -5,11 +5,11 @@ include 'header.php';
     <div id="login" class="container text-center">
         <form id="login-form" class="align-middle mx-auto" action="" method="post" enctype="multipart/formdata">
             <div class="alert alert-danger" style="display: none;">Authentication Failed</div>
-            <label for="username" class="col col-4">Username: </label>
-            <input id="username" class="col col-7" type="text" name="username" value="" /><br />
-            <label for="password" class="col col-4">Password:  </label>
-            <input id="password" class="col col-7" type="password" name="password" value="" /><br />
-            <input type="submit" name="submit" value="Sign in" class="btn btn-primary" />
+            <label for="username" class="col col-3">Username: </label>
+            <input id="username" class="col col-8" type="text" name="username" value="" /><br />
+            <label for="password" class="col col-3">Password:  </label>
+            <input id="password" class="col col-8" type="password" name="password" value="" /><br /><br />
+            <input type="submit" name="submit" value="Sign in" class="btn btn-primary" style="width: 85%;" />
         </form>
     </div>
 <?php include 'footer.php'; ?>
