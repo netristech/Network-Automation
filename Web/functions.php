ddf875a102e211b34a2076f9b531ed96afc0d189
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-17 10:21:50 -0500

    Testing authentication

--- Web/functions.php
+++ Web/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+include('Net/SSH2.php');
+
 if(isset($_POST['action']) && !empty($_POST['action'])) {
     $action = $_POST['action'];
     switch($action) {
@@ -13,12 +15,17 @@ function processLogin() {
     $server = $_POST['server'];
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $output = null;
-    $return_var = null;
-    //$command = escapeshellcmd("/usr/bin/python3 ./auth.py " . $username . " " . $password);
-    //$command = escapeshellcmd("sshpass -p " . $password . " ssh " . $username . "@" . $server . " 'exit'");
+    $ssh = new Net_SSH2($server);
+    if(!$ssh->login($username, $password)) {
+        echo 1;
+    } else {
+        $ssh->exec('exit');
+        echo 0;
+    }
+    /*$command = escapeshellcmd("/usr/bin/python3 ./auth.py " . $username . " " . $password);
+    $command = escapeshellcmd("sshpass -p " . $password . " ssh " . $username . "@" . $server . " 'exit'");
     $command = escapeshellcmd('sshpass --help');
-    //$output = shell_exec($command);
+    $output = shell_exec($command);
     exec($command, $output, $return_var);
-    print_r($output);
+    print_r($output);*/
 }
