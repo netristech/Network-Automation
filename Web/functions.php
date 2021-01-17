e1c0773cc54e31ad880d93b4bba004e8d3d4d507
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-17 10:04:59 -0500

    Testing authentication

--- Web/functions.php
+++ Web/functions.php
@@ -16,7 +16,8 @@ function processLogin() {
     $output = null;
     $return_var = null;
     //$command = escapeshellcmd("/usr/bin/python3 ./auth.py " . $username . " " . $password);
-    $command = escapeshellcmd("sshpass -p " . $password . " ssh " . $username . "@" . $server . " 'exit'");
+    //$command = escapeshellcmd("sshpass -p " . $password . " ssh " . $username . "@" . $server . " 'exit'");
+    $command = escapeshellcmd('ls -lah');
     //$output = shell_exec($command);
     exec($command, $output, $return_var);
     print_r($output);
