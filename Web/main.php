25193dcdb0a08051d26f9c127941281385a2f0c6
Brian Dunham <brian.dunham@bbraunusa.com>
2021-01-25 14:47:19 -0500

    Implement phpseclib 1.0

--- Web/main.php
+++ Web/main.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
-$_SESSION['login'] = $_POST['login'];
-if($_SESSION['login']) {
-    include 'header.php';
-    include 'importmodal.php';
-    include 'nav.php';
-    include 'sites.php';
-    include 'footer.php';
+if(isset($_POST['login'])) {
+    $_SESSION['login'] = $_POST['login'];
+    if($_SESSION['login']) {
+        include 'header.php';
+        include 'importmodal.php';
+        include 'nav.php';
+        include 'sites.php';
+        include 'footer.php';
+    }
+} else {
+    echo 'login not set';
 }
 ?>
